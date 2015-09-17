9affed077c087256b1125eafbe58feb11401e13f
Diego Dompe <dompe@hp.com>
2015-09-17 10:08:00 -0600

    Use rtd proxy on static
    Change-Id: I6161202375af1da9450df72a28956cc0757cd261
    Signed-off-by: Diego Dompe <dompe@hp.com>

--- modules/openstack_project/manifests/static.pp
+++ modules/openstack_project/manifests/static.pp
@@ -282,19 +282,18 @@ class openstack_project::static (
   ###########################################################
   # Docs
 
-  apache::vhost { 'docs.openswitch.net':
+  apache::vhost::proxy { 'docs.openswitch.net':
     port     => 80,
+    dest     => 'http://127.0.0.1:8000',
     priority => '50',
-    docroot  => '/srv/static/docs',
-    require  => File['/srv/static/docs'],
   }
 
-  file { '/srv/static/docs':
-    ensure  => directory,
-    owner   => 'jenkins',
-    group   => 'jenkins',
-    require => User['jenkins'],
-  }
+#  file { '/srv/static/docs':
+#    ensure  => directory,
+#    owner   => 'jenkins',
+#    group   => 'jenkins',
+#    require => User['jenkins'],
+#  }
 
   ###########################################################
   # Specs
