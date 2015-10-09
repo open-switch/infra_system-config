d8de6267b7f286fb82300f2c15a016d37489e758
Diego Dompe <dompe@hpe.com>
2015-10-09 07:43:33 -0600

    Configure the eavesdrop server
    Change-Id: Id293428f9868ec81a86ee53e25d680bd7f101b39
    Signed-off-by: Diego Dompe <dompe@hpe.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -273,20 +273,20 @@ node 'planet.openstack.org' {
 }
 
 # Node-OS: precise
-node 'eavesdrop.openstack.org' {
+node 'eavesdrop.openswitch.net' {
   class { 'openstack_project::eavesdrop':
     project_config_repo     => 'https://review.openswitch.net/infra/project-config',
-    nickpass                => hiera('openstack_meetbot_password', 'XXX'),
+    nickpass                => hiera('openswitch_meetbot_password', 'XXX'),
     sysadmins               => hiera('sysadmins', []),
-    statusbot_nick          => hiera('statusbot_nick', 'username'),
-    statusbot_password      => hiera('statusbot_nick_password', 'XXX'),
-    statusbot_server        => 'chat.freenode.net',
-    statusbot_channels      => 'edeploy, fuel-dev, heat, magnetodb, murano, openstack, openstack-101, openstack-anvil, openstack-bacon, openstack-barbican, openstack-blazar, openstack-board, openstack-ceilometer, openstack-chef, openstack-cinder, openstack-cloudkeep, openstack-community, openstack-containers, openstack-dev, openstack-dns, openstack-doc, openstack-entropy, openstack-foundation, openstack-gantt, openstack-gate, openstack-hyper-v, openstack-infra, openstack-ironic, openstack-keystone, openstack-manila, openstack-meeting, openstack-meeting-3, openstack-meeting-4, openstack-meeting-alt, openstack-meniscus, openstack-merges, openstack-metering, openstack-monasca, openstack-neutron, openstack-nova, openstack-opw, openstack-oslo, openstack-qa, openstack-raksha, openstack-rating, openstack-relmgr-office, openstack-sahara, openstack-sdks, openstack-sprint, openstack-stable, openstack-state-management, openstack-swift, openstack-translation, openstack-trove, openstack-zaqar, packstack-dev, refstack, storyboard, syscompass, tripleo',
-    statusbot_auth_nicks    => 'jeblair, ttx, fungi, mordred, clarkb, sdague, SergeyLukjanov, jhesketh, lifeless',
-    statusbot_wiki_user     => hiera('statusbot_wiki_username', 'username'),
-    statusbot_wiki_password => hiera('statusbot_wiki_password', 'XXX'),
-    statusbot_wiki_url      => 'https://wiki.openstack.org/w/api.php',
-    statusbot_wiki_pageid   => '1781',
+#    statusbot_nick          => hiera('statusbot_nick', 'username'),
+#    statusbot_password      => hiera('statusbot_nick_password', 'XXX'),
+#    statusbot_server        => 'chat.freenode.net',
+#    statusbot_channels      => 'edeploy',
+#    statusbot_auth_nicks    => '',
+#    statusbot_wiki_user     => hiera('statusbot_wiki_username', 'username'),
+#    statusbot_wiki_password => hiera('statusbot_wiki_password', 'XXX'),
+#    statusbot_wiki_url      => 'https://wiki.openstack.org/w/api.php',
+#    statusbot_wiki_pageid   => '1781',
     accessbot_nick          => hiera('accessbot_nick', 'username'),
     accessbot_password      => hiera('accessbot_nick_password', 'XXX'),
   }
