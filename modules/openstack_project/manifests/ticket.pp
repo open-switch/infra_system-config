1136f517d418e0a02e363ff3374550a93d355f3a
Jenna Hung <jenna.hung@hpe.com>
2016-02-03 10:48:01 -0800

    test adding local user for ticket instance
    Change-Id: I18d2c4b69f6648b5d3526b1eef23f7bd79e283a4

--- modules/openstack_project/manifests/ticket.pp
+++ modules/openstack_project/manifests/ticket.pp
@@ -8,16 +8,25 @@ class openstack_project::ticket {
     ensure => present,
     source => $::openstack_project::params::login_defs,
   }
+  # global users
   User::Virtual::Localuser {
     require => File['/etc/login.defs']
   }
 
-  @user::virtual::localuser { 'michael':
-    realname => 'Michael Marth',
-    sshkeys  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCb4Ssy/U8pHXIFV6oalxGh5hXuCIS/4YYijyUnbf8KX7tGB0kZVhdCA92ztZGuUlqbDSu2sCFwPKllNqanKDW+9Z6pClGDmIToc6hmMovkAarY4hVtvIZRUKDnhzi56kegXpxbMqPHeEWXQLM8S6GOYScPeLFFyY+8+eRQjsliTnIBobPVHs/dDSNey7WkfH4/O/BSjpmvBiGdrssL4WQhQtyX0V7J7iAv3l4V/OBHjI+tJjSlNZhGLVruoSQd9vhX1rx3hOoP+HWnBLh/kDuMXA+oUDzbuofNF34twXMMlMBnWQFz/DeA+2zxbJSSv6GH6BMH9euWC3bB7Qq6Pk9d',
-    key_id   => 'michael.marth@hpe.com',
-    uid      => 3000,
-    gid      => 3000,
+  # local users
+  user { 'michael':
+    ensure => present,
+    home => '/home/michael',
+    groups  => ['sudo'],
+    shell   => '/bin/bash',
+    uid     => '3000',
+    gid     => '3000',
+    managehome => true
   }
 
+  ssh_authorized_key { 'michael_key':
+  user => 'michael',
+  type => 'rsa',
+  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCb4Ssy/U8pHXIFV6oalxGh5hXuCIS/4YYijyUnbf8KX7tGB0kZVhdCA92ztZGuUlqbDSu2sCFwPKllNqanKDW+9Z6pClGDmIToc6hmMovkAarY4hVtvIZRUKDnhzi56kegXpxbMqPHeEWXQLM8S6GOYScPeLFFyY+8+eRQjsliTnIBobPVHs/dDSNey7WkfH4/O/BSjpmvBiGdrssL4WQhQtyX0V7J7iAv3l4V/OBHjI+tJjSlNZhGLVruoSQd9vhX1rx3hOoP+HWnBLh/kDuMXA+oUDzbuofNF34twXMMlMBnWQFz/DeA+2zxbJSSv6GH6BMH9euWC3bB7Qq6Pk9d',
+  }
 }
