418a4aa9d3db9d765af231230a662e922f0d7ce6
David Ramirez <dramirezp@hpe.com>
2016-02-09 14:05:31 -0600

    Decrease the time to clean slaves from 12 to 6 hours
    Change-Id: Ic0bb3ce9db98d64a1c5861b6efcb83ab8e82dc4d
    Signed-off-by: David Ramirez <dramirezp@hpe.com>

--- modules/openstack_project/manifests/thick_slave.pp
+++ modules/openstack_project/manifests/thick_slave.pp
@@ -156,7 +156,7 @@ class openstack_project::thick_slave(
   cron{ 'cleanup_jenkins':
     command => '/usr/sbin/cleanup_jenkins.sh | /usr/bin/logger -t cleanup_jenkins',
     user => 'root',
-    hour => "*/12",
+    hour => "*/6",
     minute => "0",
   }
 
