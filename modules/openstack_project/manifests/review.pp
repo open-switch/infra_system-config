f0c54bc915a75e8d484774706da887af67fece2c
Tyler Heucke <tyler.heucke@dell.com>
2017-12-27 16:27:03 -0800

    Upgrade gerrit from 2.11.4 to 2.13.9
    Change-Id: I936f8616ec5213ad849783c7391882c2d19d0c40

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -125,7 +125,7 @@ class openstack_project::review (
     sshd_threads                        => '100',
     httpd_maxwait                       => '5000min',
     war                                 =>
-      'http://tarballs.openstack.org/ci/gerrit/gerrit-v2.11.4.23.12d5c47.war',
+      'http://tarballs.openstack.org/ci/gerrit/gerrit-v2.13.9.4.2a605d5.war',
     contactstore                        => $contactstore,
     contactstore_appsec                 => $contactstore_appsec,
     contactstore_pubkey                 => $contactstore_pubkey,
