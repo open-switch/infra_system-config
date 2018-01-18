db4e4d1438fac1a91651b3c8caa1d87da34d1f54
Tyler Heucke <tyler.heucke@dell.com>
2018-01-18 00:39:42 +0000

    Revert "Add gerrit reviewers plugin"
    This reverts commit 1cd428c8786dc491143af25e5f0cb66a2b060985.
    
    Change-Id: I93802999d4d3df2cad42f6072c4b8118f3b34ca3

--- modules/openstack_project/manifests/review.pp
+++ modules/openstack_project/manifests/review.pp
@@ -208,11 +208,6 @@ class openstack_project::review (
     require                             => $::project_config::config_dir,
   }
 
-  gerrit::plugin { 'reviewers':
-    version => '2.13',
-    base_url => 'https://archive.openswitch.net/old/gerrit/',
-  }
-
   gerrit::plugin { 'javamelody':
     version => 'e00d5af',
   }
@@ -222,7 +217,7 @@ class openstack_project::review (
   }
 
   gerrit::plugin { 'gerrit-oauth-provider':
-    base_url => "https://archive.openswitch.net/old/gerrit/",
+    base_url => "https://archive.openswitch.net/gerrit/",
     version => '0.3',
   }
 
